2fd901631aeb0a1c9295226256e7064119dca0f7
VisionaryLion <kylebrennan@radiogenicgames.com>
2018-01-23 18:24:47 +0100

    attempt to build v1002, failing with an error

---
+++ Assets/Main/Scripts/Utility/loadDemoEnd.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadDemoEnd : MonoBehaviour {
+
+    //variable for checking if the trigger is entered
+    bool TriggerEntered;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    //checks if the player enters the trigger
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //only sets the trigger variable to true if it's the player
+        if (other.tag == "Player")
+        {
+            TriggerEntered = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //only works if the variable is set to true
+        if (TriggerEntered)
+        {
+            //loads the scene
+            SceneManager.LoadScene("DemoEnd"); //loads assigned scene
+        }
+    }
+}
