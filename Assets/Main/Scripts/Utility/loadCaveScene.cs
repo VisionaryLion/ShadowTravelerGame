d49363c278666210868044c17e8cec4d7f1e33aa
VisionaryLion <kylebrennan@radiogenicgames.com>
2018-02-02 16:19:34 +0100

    updated level trigger scripts

---
+++ Assets/Main/Scripts/Utility/loadCaveScene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadCaveScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //load the cave scene
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //only load the scene if the object is tagged as player
+        if (other.tag == "Player")
+        {
+            SceneManager.LoadScene("TheCave"); //loads assigned scene
+        }
+    }
+}
