1139e359649ce3ce50978a651b023d76fc5beb30
VisionaryLion <kylebrennan@radiogenicgames.com>
2018-04-02 15:40:13 +0200

    Added temp energy gun pickup

---
+++ Assets/Main/Scripts/Items/energyGun.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class energyGun : MonoBehaviour {
+
+    //item variables
+    public GameObject playerGun; //reference to the player's gun
+    public Transform gunArm; //reference to the where the player's gun will appear
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //only trigger if the object is tagged as player
+        if (other.tag == "Player")
+        {
+            Instantiate(playerGun, gunArm.transform); //instantiate the torch at the torch arm's location with 0 rotation
+            Destroy(gameObject); //destroys the gun object
+
+        }
+    }
+}
