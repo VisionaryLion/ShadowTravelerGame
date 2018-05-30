e9f19a11f405887f3eb9e0d07ece5a2ba701220f
VisionaryLion <kylebrennan@radiogenicgames.com>
2018-05-30 11:47:29 +0200

    dark enemy collides with lights and can't enter

--- Assets/Main/Scripts/Enemies/enemyController.cs
+++ Assets/Main/Scripts/Enemies/enemyController.cs
@@ -26,14 +26,41 @@ public class enemyController : MonoBehaviour {
     public float stoppingDistance; //higher the number the further away the enemy will stop from the light
     public float retreatDistance; //when the enemy should back away from the light
 
+    //for waypoint walking
+    public List<GameObject> WaypointPositions;
+    public float speed = 1;
 
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    private int currentWaypoint = 0;
+
+    private Vector3 targetPositionDelta;
+    private Vector3 moveDirection = Vector3.zero;
+
+    private float horizontalPositionOld;
+    private bool facingRight = true;
+
+    [Tooltip("Default=0 - Set a value in seconds, how long the enemy should wait to move")]
+    public float StartWalkingAfterSeconds = 0;
+
+    private float startWalkingTime;
+    private bool doStartWalking = false;
+    private float gametime;
+
+    //start walking on awake
+    void Awake()
+    {
+
+        // Get the current time
+        startWalkingTime = Time.deltaTime + StartWalkingAfterSeconds;
+
+        // If default = 0 do walking
+        if (StartWalkingAfterSeconds == 0)
+        {
+            doStartWalking = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
 
         //detect when light is in range
         lightInRange = Physics2D.OverlapCircle(transform.position, lightCheckRadius, lightLayer);
@@ -44,46 +71,112 @@ public class enemyController : MonoBehaviour {
         //find the player object
         player = GameObject.FindGameObjectWithTag("Player").transform; //finds the position of the object tagged player
 
-        //dark enemy behavior
-        if (afraidofDark)
+        //for walking
+        gametime += Time.deltaTime;
+
+        if (startWalkingTime <= gametime)
         {
-            if (playerInRange && !lightInRange)
-            {
-                transform.position = Vector2.MoveTowards(transform.position, player.position, followSpeed * Time.deltaTime); //the enemy moves closer to his target           
-            }
-
-            else if (playerInRange && lightInRange && Vector2.Distance(transform.position, player.position) > retreatDistance)
-            {
-                transform.position = this.transform.position; //stops the enemy moving
-            }
-
-            else if (playerInRange && lightInRange && Vector2.Distance(transform.position, player.position) < retreatDistance)
-            {
-                transform.position = Vector2.MoveTowards(transform.position, player.position, -followSpeed * Time.deltaTime); //the enemy moves away from his target
-            }
-        }
-
-        //light enemy behavior
-        if (afraidofLight)
-        {
-            if (playerInRange && lightInRange)
-            {
-                transform.position = Vector2.MoveTowards(transform.position, player.position, followSpeed * Time.deltaTime); //the enemy moves closer to his target           
-            }
-
-            else if (playerInRange && !lightInRange && Vector2.Distance(transform.position, player.position) > retreatDistance)
-            {
-                transform.position = this.transform.position; //stops the enemy moving
-            }
-
-            else if (playerInRange && !lightInRange && Vector2.Distance(transform.position, player.position) < retreatDistance)
-            {
-                transform.position = Vector2.MoveTowards(transform.position, player.position, -followSpeed * Time.deltaTime); //the enemy moves away from his target
-            }
+            doStartWalking = true;
         }
 
     }
 
+    void FixedUpdate()
+    {
+
+        if (doStartWalking)
+        {
+
+            // Save actual position for flip-check
+            horizontalPositionOld = transform.position.x;
+
+            //dark enemy behavior
+            if (afraidofLight)
+            {
+                if (playerInRange && !lightInRange)
+                {
+                    transform.position = Vector2.MoveTowards(transform.position, player.position, followSpeed * Time.deltaTime); //the enemy moves closer to his target           
+                }
+
+                else if (playerInRange && lightInRange && Vector2.Distance(transform.position, player.position) > retreatDistance)
+                {
+                    transform.position = this.transform.position; //stops the enemy moving
+                }
+
+                else if (playerInRange && lightInRange && Vector2.Distance(transform.position, player.position) < retreatDistance)
+                {
+                    transform.position = Vector2.MoveTowards(transform.position, player.position, -followSpeed * Time.deltaTime); //the enemy moves away from his target
+                }
+
+                else if (!playerInRange)
+                {
+                    WaypointWalk();
+                    Move();
+                }
+            }
+
+            // Flip-Check
+            if (transform.position.x < horizontalPositionOld)
+            {
+
+                if (facingRight)
+                {
+                    Flip();
+                    facingRight = false;
+                }
+            }
+            else
+            {
+                if (!facingRight)
+                {
+                    Flip();
+                    facingRight = true;
+                }
+            }
+        }
+    }
+
+    void WaypointWalk()
+    {
+
+        if (WaypointPositions.Count > 0)
+        {
+
+            GameObject wp = (GameObject)WaypointPositions[currentWaypoint];
+            Vector3 targetPosition = wp.transform.position;
+
+            targetPositionDelta = targetPosition - transform.position;
+
+            // if i´m near the next waypoint count one high
+            if (targetPositionDelta.sqrMagnitude <= 0.01f)
+            {
+
+                currentWaypoint++;
+
+                // If the last waypoint reached, start again
+                if (currentWaypoint >= WaypointPositions.Count)
+                {
+                    currentWaypoint = 0;
+                }
+            }
+        }
+    }
+
+    protected virtual void Move()
+    {
+        moveDirection = targetPositionDelta.normalized * speed;
+        transform.Translate(moveDirection * Time.deltaTime, Space.World);
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+    //make the light and player detection radius' visible in the editor
     void OnDrawGizmosSelected()
     {
 
