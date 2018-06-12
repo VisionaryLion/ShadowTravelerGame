d0afd3d629dfa132b449e55f3c13a70c71bb1b9a
VisionaryLion <kylebrennan@radiogenicgames.com>
2018-06-12 17:18:59 +0200

    implemented health lighting and dead body prototype

--- Assets/Other/Tutorials/2D Prototyping in Unity/Scripts/playerHealth.cs
+++ Assets/Other/Tutorials/2D Prototyping in Unity/Scripts/playerHealth.cs
@@ -14,17 +14,29 @@ public class playerHealth : MonoBehaviour {
 
     playerController controlMovement;
 
+    public GameObject deadPlayer;
+
     //HUD Variables
     public Slider healthSlider;
-    
+    public Image Fill;
+    public Color MaxHealthColor = Color.green;
+    public Color MinHealthColor = Color.red;
+    public SFLight HeadLight;
+    public SFLight ChestLight;
+    public SFLight HealthLight;
+
     // Use this for initialization
-	void Start () {
+    void Start () {
         currentHealth = fullHealth;
         controlMovement = GetComponent<playerController>();
 
         //HUD Initilization
         healthSlider.maxValue = fullHealth; //sets the max value of the health slider to the max health of the player
         healthSlider.value = fullHealth; //sets the starting value of the health slider to the full health of the player
+        Fill.color = MaxHealthColor;
+        HeadLight.color = MaxHealthColor;
+        ChestLight.color = MaxHealthColor;
+        HealthLight.color = MaxHealthColor;
 
         //initial player respawn point
         respawnPoint = transform.position;
@@ -40,17 +52,27 @@ public class playerHealth : MonoBehaviour {
         Instantiate(deathFX, transform.position, transform.rotation); //spawns the death particle effects
         currentHealth -= damage;
         healthSlider.value = currentHealth;
+        Fill.color = Color.Lerp(MinHealthColor, MaxHealthColor, currentHealth / fullHealth);
+        HeadLight.color = Color.Lerp(MinHealthColor, MaxHealthColor, currentHealth / fullHealth);
+        ChestLight.color = Color.Lerp(MinHealthColor, MaxHealthColor, currentHealth / fullHealth);
+        HealthLight.color = Color.Lerp(MinHealthColor, MaxHealthColor, currentHealth / fullHealth);
 
         if (currentHealth <= 0) {
             makeDead();
         }
+
     }
 
     public void makeDead() {
         Instantiate(deathFX, transform.position, transform.rotation); //spawns the death particle effects
+        Instantiate(deadPlayer, transform.position, transform.rotation); //spawns the dead player object
         transform.position = respawnPoint; //moves the player to the position of the currnet checkpoint
         currentHealth = fullHealth;
         healthSlider.value = currentHealth;
+        Fill.color = MaxHealthColor; //resets the fill color back to the max after the player dies
+        HeadLight.color = MaxHealthColor;
+        ChestLight.color = MaxHealthColor;
+        HealthLight.color = MaxHealthColor;
         //Destroy(gameObject); //destroys the player
         //SceneManager.LoadScene("CaveTilemap"); //reloads the scene
     }
